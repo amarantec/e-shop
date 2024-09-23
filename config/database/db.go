b88c7e99c71d56c4a5a16861dfc47f56595836f1
amarantec <marcoscarvalhop@outlook.com>
2024-09-22 23:35:07 -0300

    change db for postgres

--- config/database/db.go
+++ config/database/db.go
@@ -1,6 +1,11 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
+	usermodel "github.com/amarantec/e-shop/models/user_model"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -8,8 +13,20 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("test,db"), &gorm.Config{})
+
+	host := os.Getenv("DB_HOST")
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	databaseName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s  user=%s  password=%s dbname=%s port=%s sslmode=disable  TimeZone=America/Sao_Paulo", host, username, password, databaseName, port)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database")
 	}
+
+	DB.AutoMigrate(
+		&usermodel.User{},
+	)
 }
