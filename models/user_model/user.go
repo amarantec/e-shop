737df600d5ab054f4ac20c4e9342905dc317ef1e
amarantec <marcoscarvalhop@outlook.com>
2024-09-22 23:40:25 -0300

    add required bind

--- models/user_model/user.go
+++ models/user_model/user.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name" gorm:"not null"`
-	Email    string `json:"email" gorm:"unique, not null"`
-	Password string `json:"password" gorm:"not null"`
+	Name     string `json:"name"  binding:"required" gorm:"not null"`
+	Email    string `json:"email" binding:"required" gorm:"not null; unique"`
+	Password string `json:"password" binding:"required" gorm:"not null"`
 }
