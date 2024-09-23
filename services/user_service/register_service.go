a08c13dde7a0d1695693e44ff8b5a197c372715f
amarantec <marcoscarvalhop@outlook.com>
2024-09-22 23:42:39 -0300

    add validations for user input

--- services/user_service/register_service.go
+++ services/user_service/register_service.go
@@ -1,7 +1,35 @@
 package userservice
 
-import usermodel "github.com/amarantec/project777/models/user_model"
+import (
+	"context"
 
-func (s *userService) Register(user *usermodel.User) error {
-	return s.userRepo.Register(user)
+	"github.com/amarantec/e-shop/models"
+	usermodel "github.com/amarantec/e-shop/models/user_model"
+)
+
+func (s *userService) Register(ctx context.Context, user *usermodel.User) (models.Response[uint], error) {
+	response := models.Response[uint]{}
+
+	if validName, err := validUserName(user.Name); err != nil || !validName {
+		response.Success = false
+		return response, ErrUserNameInvalidFormat
+	}
+
+	if validEmail, err := validUserEmail(user.Email); err != nil || !validEmail {
+		response.Success = false
+		return response, ErrUserEmailInvalidFormat
+	}
+
+	if validPassword, err := validUserPassword(user.Password); err != nil || !validPassword {
+		response.Success = false
+		return response, ErrUserPasswordInvalidFormat
+	}
+
+	response, err := s.userRepo.Register(ctx, user)
+	if err != nil {
+		response.Success = false
+		return response, err
+	}
+
+	return response, nil
 }
