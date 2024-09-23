8331b17f0dd8e6b22f3d8b21b8cbd70011b5ca54
amarantec <marcoscarvalhop@outlook.com>
2024-09-22 23:38:41 -0300

    add hash function for password security

--- repositories/user_repository/register_repository.go
+++ repositories/user_repository/register_repository.go
@@ -1,10 +1,34 @@
 package userrepository
 
 import (
-	"github.com/amarantec/project777/config/database"
-	usermodel "github.com/amarantec/project777/models/user_model"
+	"context"
+
+	"github.com/amarantec/e-shop/config/database"
+	"github.com/amarantec/e-shop/models"
+	usermodel "github.com/amarantec/e-shop/models/user_model"
+	"github.com/amarantec/e-shop/utils"
 )
 
-func (r *userRepository) Register(user *usermodel.User) error {
-	return database.DB.Create(user).Error
+func (r *userRepository) Register(ctx context.Context, user *usermodel.User) (models.Response[uint], error) {
+	response := models.Response[uint]{}
+
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		response.Success = false
+		response.Message = "error when hashing password"
+		return response, err
+	}
+
+	user.Password = hashedPassword
+
+	if err := database.DB.WithContext(ctx).
+		Create(user).Error; err != nil {
+		response.Success = false
+		return response, err
+	}
+
+	response.Data = user.ID
+	response.Success = true
+	response.Message = "User registred successfully"
+	return response, nil
 }
