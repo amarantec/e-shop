3adb15d70d3cf2c4cd2b92e2aaac09721b33b091
amarantec <marcoscarvalhop@outlook.com>
2024-09-22 23:36:18 -0300

    add context for connections

--- controllers/user_controller/register_controller.go
+++ controllers/user_controller/register_controller.go
@@ -1,25 +1,30 @@
 package usercontroller
 
 import (
+	"context"
 	"net/http"
+	"time"
 
-	usermodel "github.com/amarantec/project777/models/user_model"
+	usermodel "github.com/amarantec/e-shop/models/user_model"
 	"github.com/gin-gonic/gin"
 )
 
 func (ctrl *UserController) Register(c *gin.Context) {
 	var newUser usermodel.User
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	if err := c.ShouldBindJSON(&newUser); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "could not parse this request"})
 		return
 	}
 
-	if err := ctrl.service.Register(&newUser); err != nil {
+	res, err := ctrl.service.Register(ctxTimeout, &newUser)
+	if err != nil {
 		c.JSON(http.StatusInternalServerError,
-			gin.H{"error": "Could not register user"})
+			gin.H{"error": "could not register user"})
 		return
 	}
 
-	c.JSON(http.StatusCreated, newUser)
+	c.JSON(http.StatusCreated, res)
 }
