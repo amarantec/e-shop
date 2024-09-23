abfed0ddcf86a3cdb365bffc11014acc50d6e6b0
amarantec <marcoscarvalhop@outlook.com>
2024-09-22 23:43:52 -0300

    new route

--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 func SetupRoutes(r *gin.Engine) {
 	userGroup := r.Group("/users")
 	{
-		userGroup.POST("/", userCtrl.Register)
+		userGroup.POST("/register", userCtrl.Register)
+		userGroup.POST("/login", userCtrl.Login)
 	}
 }
