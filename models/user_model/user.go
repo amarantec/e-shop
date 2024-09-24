7e50e189cbf932bcaaf15f7fe89fc724c80cfcc3
amarantec <marcoscarvalhop@outlook.com>
2024-09-23 21:42:49 -0300

    add address field

--- models/user_model/user.go
+++ models/user_model/user.go
@@ -1,10 +1,14 @@
 package usermodel
 
-import "gorm.io/gorm"
+import (
+	addressmodel "github.com/amarantec/e-shop/models/address_model"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name"  binding:"required" gorm:"not null"`
-	Email    string `json:"email" binding:"required" gorm:"not null; unique"`
-	Password string `json:"password" binding:"required" gorm:"not null"`
+	Name     string                 `json:"name"  binding:"required" gorm:"not null"`
+	Email    string                 `json:"email" binding:"required" gorm:"not null; unique"`
+	Password string                 `json:"password" binding:"required" gorm:"not null"`
+	Address  []addressmodel.Address `json:"address" gorm:"foreignKey:UserId"`
 }
