967b3b26917d60e461dcb32a9b8315a65d9a5fb9
amarantec <marcoscarvalhop@outlook.com>
2024-09-23 21:21:50 -0300

    add new funcs

--- repositories/user_repository/user_repository.go
+++ repositories/user_repository/user_repository.go
@@ -8,8 +8,10 @@ import (
 )
 
 type UserRepository interface {
-	Register(ctx context.Context, user *usermodel.User) (models.Response[uint], error)
-	FindUser(ctx context.Context, email string) (usermodel.User, error)
+	SaveUser(ctx context.Context, user *usermodel.User) (models.Response[uint], error)
+	FindUserByEmail(ctx context.Context, email string) (usermodel.User, error)
+	FindUserById(ctx context.Context, userId uint) (usermodel.User, error)
+	ChangePassword(ctx context.Context, userId uint, password string) error
 }
 
 type userRepository struct{}
