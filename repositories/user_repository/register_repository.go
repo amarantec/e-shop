e3d3054d3d4979bacb43ccbfcabeb753f199cbba
amarantec <marcoscarvalhop@outlook.com>
2024-09-23 21:21:23 -0300

    move hash func to service

--- repositories/user_repository/register_repository.go
+++ repositories/user_repository/register_repository.go
@@ -6,20 +6,10 @@ import (
 	"github.com/amarantec/e-shop/config/database"
 	"github.com/amarantec/e-shop/models"
 	usermodel "github.com/amarantec/e-shop/models/user_model"
-	"github.com/amarantec/e-shop/utils"
 )
 
-func (r *userRepository) Register(ctx context.Context, user *usermodel.User) (models.Response[uint], error) {
+func (r *userRepository) SaveUser(ctx context.Context, user *usermodel.User) (models.Response[uint], error) {
 	response := models.Response[uint]{}
-
-	hashedPassword, err := utils.HashPassword(user.Password)
-	if err != nil {
-		response.Success = false
-		response.Message = "error when hashing password"
-		return response, err
-	}
-
-	user.Password = hashedPassword
 
 	if err := database.DB.WithContext(ctx).
 		Create(user).Error; err != nil {
