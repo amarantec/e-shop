5b26ba9902b5b554b56c066fce4c391aa65bdb90
amarantec <marcoscarvalhop@outlook.com>
2024-09-23 21:28:57 -0300

    change func name && add middleware func to authorize user

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,15 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/amarantec/e-shop/middleware"
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRoutes(r *gin.Engine) {
 	userGroup := r.Group("/users")
 	{
-		userGroup.POST("/register", userCtrl.Register)
+		userGroup.POST("/register", userCtrl.SaveUser)
 		userGroup.POST("/login", userCtrl.Login)
+		userGroup.POST("/change-password/:password", middleware.Auth, userCtrl.ChangePassword)
 	}
 }
