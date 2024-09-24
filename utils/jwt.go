157a72ac7ffe0488307b2d6a1f08cba9806c1851
amarantec <marcoscarvalhop@outlook.com>
2024-09-23 21:29:31 -0300

    refactor jwt token funcs

--- utils/jwt.go
+++ utils/jwt.go
@@ -3,60 +3,58 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
-	"strings"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
 
 var privateKey = []byte(os.Getenv("JWT_PRIVATE_KEY"))
 
-func GenerateUserToken(userId uint, email string) (string, error) {
+func GenerateToken(userId uint, email string) (string, error) {
 	if userId == 0 {
-		return "", fmt.Errorf("jwt error: user id is empty")
-	} else if email == "" {
-		return "", fmt.Errorf("jwt error: email is empty")
-	} else {
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-			"email":  email,
-			"userId": userId,
-			"exp":    time.Now().Add(time.Hour * 2).Unix(),
-		})
-		return token.SignedString([]byte(privateKey))
-	}
-}
-
-func ValidateUserToken(c *gin.Context) error {
-	token, err := getToken(c)
-	if err != nil {
-		return err
+		return "", fmt.Errorf("jwt error: token id is empty")
 	}
 
-	_, ok := token.Claims.(jwt.MapClaims)
-	if ok && token.Valid {
-		return nil
-	}
-	return errors.New("invalid token provided")
-}
-
-func getToken(c *gin.Context) (*jwt.Token, error) {
-	tokenString := getTokenFromRequest(c)
-	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
-		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
-		}
-		return privateKey, nil
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userId": userId,
+		"exp":    time.Now().Add(time.Hour * 24).Unix(),
 	})
-	return token, err
+
+	return token.SignedString([]byte(privateKey))
 }
 
-func getTokenFromRequest(c *gin.Context) string {
-	bearerToken := c.Request.Header.Get("Authorization")
-	splitToken := strings.Split(bearerToken, "")
-	if len(splitToken) == 2 {
-		return splitToken[1]
+func VerifyToken(token string) (uint, error) {
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(privateKey), nil
+	})
+
+	if err != nil {
+		log.Printf("token err: %v", err)
+		return 0, errors.New("could not parse token")
 	}
-	return ""
+
+	if !parsedToken.Valid {
+		log.Printf("token err: %v", err)
+		return 0, errors.New("invalid token")
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("invalid token claims")
+	}
+
+	userIdFloat, ok := claims["userId"].(float64)
+	if !ok {
+		return 0, errors.New("userid claim is not a valid float64")
+	}
+
+	userId := uint(userIdFloat)
+
+	return userId, nil
 }
