acf077e5a81d2072d54c4edc1bb745c65dce87a8
amarantec <marcoscarvalhop@outlook.com>
2024-09-27 19:56:31 -0300

    foreign key model update

--- models/product_model/product_types.go
+++ models/product_model/product_types.go
@@ -4,8 +4,7 @@ import "gorm.io/gorm"
 
 type ProductTypes struct {
 	gorm.Model
-	Name     string            `json:"name" binding:"required" gorm:"not nul"`
-	Editing  bool              `json:"editing" binding:"required" gorm:"not nul"`
-	IsNew    bool              `json:"is_new" binding:"required" gorm:"not nul"`
-	Variants []ProductVariants `gorm:"foreignKey:ProductTypesID;references:ID"`
+	Name    string `json:"name" binding:"required" gorm:"not nul"`
+	Editing bool   `json:"editing" binding:"required" gorm:"not nul"`
+	IsNew   bool   `json:"is_new" binding:"required" gorm:"not nul"`
 }
--- models/product_model/product_variants.go
+++ models/product_model/product_variants.go
@@ -7,8 +7,8 @@ import (
 type ProductVariants struct {
 	gorm.Model
 	ProductID      uint         `json:"product_id" binding:"required" gorm:"not nul"`
-	ProductTypes   ProductTypes `gorm:"foreignKey:ProductTypesID"`
-	ProductTypesID uint         `json:"product_type_id" binding:"required" gorm:"not nul"`
+	ProductTypes   ProductTypes 
+	ProductTypesID uint         `json:"product_types_id" binding:"required" gorm:"not nul"`
 	Price          float64      `json:"price" binding:"required" gorm:"not nul"`
 	OriginalPrice  float64      `json:"original_price" binding:"required" gorm:"not nul"`
 	Visible        bool         `json:"visible" binding:"omitempty" gorm:"not nul"`
