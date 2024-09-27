6f21fec9a0238670e8032f1cd9b7fec944faa5bb
amarantec <marcoscarvalhop@outlook.com>
2024-09-26 22:22:18 -0300

    improve delete func

--- repositories/address_repository/delete_address_repository.go
+++ repositories/address_repository/delete_address_repository.go
@@ -2,23 +2,26 @@ package addressrepository
 
 import (
 	"context"
+	"errors"
 
 	"github.com/amarantec/e-shop/config/database"
 	"github.com/amarantec/e-shop/models"
 	addressmodel "github.com/amarantec/e-shop/models/address_model"
+	"gorm.io/gorm"
 )
 
 func (r *addressRepository) DeleteAddress(ctx context.Context, userId, addressId uint) (models.Response[bool], error) {
 	response := models.Response[bool]{}
 
-	result :=
+	if err :=
 		database.DB.WithContext(ctx).Where("user_id = ? AND id = ?", userId, addressId).
-			Delete(&addressmodel.Address{})
-
-	if result.RowsAffected == 0 {
-		response.Success = true
-		response.Message = "Address not found"
-		return response, nil
+			Delete(&addressmodel.Address{}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			response.Message = "Product not found"
+			return response, nil
+		}
+		response.Success = false
+		return response, err
 	}
 
 	response.Data = true
