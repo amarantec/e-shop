15c721bfd969651f6508a43d7de72a3635b85d82
amarantec <marcoscarvalhop@outlook.com>
2024-10-02 22:29:57 -0300

    return value refactor

--- repositories/product_repository/get_product_repository.go
+++ repositories/product_repository/get_product_repository.go
@@ -4,26 +4,20 @@ import (
 	"context"
 
 	"github.com/amarantec/e-shop/config/database"
-	"github.com/amarantec/e-shop/models"
 	productmodel "github.com/amarantec/e-shop/models/product_model"
 )
 
-func (r *productRepository) GetProduct(ctx context.Context, productId uint) (models.Response[productmodel.Product], error) {
-	response := models.Response[productmodel.Product]{}
-
+func (r *productRepository) GetProduct(ctx context.Context, productId uint) (productmodel.Product, error) {
+	product := productmodel.Product{}
 	if err := database.DB.WithContext(ctx).
 		Model(productmodel.Product{}).
 		Preload("Category").                     // Precarregar a categoria associada
 		Preload("ProductVariants.ProductTypes"). // Precarregar as variantes do produto e o ProductTypes
 		Preload("Images").                       // Precarregar as imagens do produto
 		Where("id= ?", productId).
-		First(&response.Data).Error; err != nil {
-		response.Success = false
-		response.Message = "error when getting product"
-		return response, err
+		First(&product).Error; err != nil {
+		return productmodel.Product{}, err
 	}
 
-	response.Success = true
-	response.Message = "Product retrieved successfully"
-	return response, nil
+	return product, nil
 }
--- repositories/product_repository/product_repository.go
+++ repositories/product_repository/product_repository.go
@@ -11,7 +11,7 @@ type ProductRepository interface {
 	CreateProduct(ctx context.Context, product productmodel.Product) (models.Response[productmodel.Product], error)
 	DeleteProduct(ctx context.Context, productId uint) (models.Response[bool], error)
 	ListFeaturedProducts(ctx context.Context) (models.Response[[]productmodel.Product], error)
-	GetProduct(ctx context.Context, productId uint) (models.Response[productmodel.Product], error)
+	GetProduct(ctx context.Context, productId uint) (productmodel.Product, error)
 	ListProducts(ctx context.Context) (models.Response[[]productmodel.Product], error)
 	ListProductsByCategory(ctx context.Context, categoryUrl string) (models.Response[[]productmodel.Product], error)
 	UpdateProduct(ctx context.Context, product productmodel.Product) (models.Response[bool], error)
