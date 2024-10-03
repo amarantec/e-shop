b87c1363a70b9de6a5a86db4fb3ef033a1567554
amarantec <marcoscarvalhop@outlook.com>
2024-10-02 23:19:19 -0300

    add cors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/amarantec/e-shop
 go 1.23.1
 
 require (
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/amarantec/e-shop/config/database"
 	"github.com/amarantec/e-shop/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -14,7 +16,15 @@ func main() {
 	database.ConnectDatabase()
 
 	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
 
+	r.Use(cors.New(config))
 	routes.SetupRoutes(r)
 
 	r.Run(":8080")
